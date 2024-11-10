a4f388deca01a8e669d750fe38016452d395f223
charbara <charbara@LAPTOP-10FTCSDB.>
2024-11-10 01:38:56 +0100

    adding a hero simple section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { useEffect,useState } from "react";
-
+import './css/hero.css'
 function Hero(){
-    return (<div>
-        <h1> hello wolrd</h1>
+    return (
+    <div className="background">
+        <div className="filter">
+        <h1>hello world</h1>
+        </div>
     </div>)
 }
+
+
+
+export default Hero
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import TopBar from './Topbar.jsx'
-
+import Hero from './Hero.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <TopBar/>
+    <Hero/>
   </StrictMode>,
 )
